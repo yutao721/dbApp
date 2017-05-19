9e06e16599d94e22997ef170574c7acb15b856b0
yutao <yt@gdysj.com.cn>
2017-05-19 18:14:08 +0800

    s

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -6,33 +6,49 @@
 </template>
 
 <script>
-export default {
-  name: 'hello',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
+  import axios from 'axios';
+  export default {
+    name: 'hello',
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        movieLists: []
+      }
+    },
+    created () {
+      axios.get('api/movie/in_theaters',
+        {'city': '广州'})
+        .then((result) => {
+//          console.log(result);
+          if (result.status === 200 && result.statusText === 'OK') {
+            this.movieLists = result.data.subjects;
+            console.log(this.movieLists)
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+  h1, h2 {
+    font-weight: normal;
+  }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-a {
-  color: #42b983;
-}
+  a {
+    color: #42b983;
+  }
 </style>
